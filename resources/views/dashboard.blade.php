f9f805605304d9d6a831c8cf68d62a5b201fcc12
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-01-27 16:51:54 -0300

    HEY-26: applying filter in the front end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,11 +11,24 @@
                 <form method="GET">
                     <x-input-label for="search" :value="__('Search')" />
                     <x-text-input id="search" class="mt-1" type="text" name="search" :value="old('search')" />
+                    <x-primary-button class="h-10 ml-3">Search</x-primary-button>
                 </form>
 
-                @foreach ($questions as $item)
-                    <x-question :question="$item" />
-                @endforeach
+                @if ($questions->isEmpty())
+                    <div class="flex flex-col justify-center text-center">
+                        <div class="flex justify-center">
+                            <x-draw.searching width="400" />
+                        </div>
+
+                        <div class="mt-6 text-2xl font-bold">
+                            Question not found
+                        </div>
+                    </div>
+                @else
+                    @foreach ($questions as $item)
+                        <x-question :question="$item" />
+                    @endforeach
+                @endif
 
                 {{ $questions->withQueryString()->links() }}
             </div>
